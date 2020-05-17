07b7515274e69a9a19d84fa095db79bc8067828e
eduardo <jose.ramirez1107@uppuebla.edu.mx>
2020-05-17 12:40:40 -0500

    prueba de actualizacion del repositorio

---
+++ Minombre/src/minombre/Minombre.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre;
+
+/**
+ *
+ * @author acteck
+ */
+public class Minombre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){ 
+            System.out.print("Mi nombre es : jose eduardo ramirez mendoza");
+        // TODO code application logic here
+    }
+    
+}
