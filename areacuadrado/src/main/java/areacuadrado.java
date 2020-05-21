f08df4523548cbc679890b292321d681ea8e494e
jose eduardo ramirez mendoza <jose.ramirez1107@uppuebla.edu.mx>
2020-05-21 08:42:32 -0500

    ejercicio area del cuadrado version 2

--- areacuadrado/src/main/java/areacuadrado.java
+++ areacuadrado/src/main/java/areacuadrado.java
@@ -9,16 +9,14 @@ public class areacuadrado {
      */
     public static void main(String[] args) {
          Scanner leer=new Scanner(System.in);
+         double base,area;
          System.out.print("calcular area   del cuadrado \n");
-         System.out.print("dame la base del cuadrado \n");
-         double base=leer.nextInt();
-         System.out.print("dame la altura del cuadrado \n");
-         double altura=leer.nextInt();
-         double area;
-         area=base*altura;
+         System.out.print("dame un lado del cuadrado \n");
+         base=leer.nextInt();
+         area=base*base;
          
         System.out.print("el area del cuadrado es:"+area);
-        // TODO code application logic here
+       
     }
     
 }
